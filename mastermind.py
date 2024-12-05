50090dfc094b13ee91d972ae9813bd77f0b575cb
Noah Vezina <noahvezina26@gmail.com>
2024-12-05 12:21:00 -0500

    finalized game

--- mastermind.py
+++ mastermind.py
@@ -2,20 +2,22 @@ import random
 
 def main_loop(sequence):
 
+    print(f"The possible colours are {colours}.")
+
     guess_count = 1
 
     while guess_count <= 12:
 
-        guess = input("Please guess a sequence of colours (ex. 'RGBY'): ")
+        guess = input("Please guess a sequence of colours (ex. 'R G B Y'): ").split()
 
         correct_exact = check_exact(guess)
-        correct_colours = check_colours(guess)
+        correct_colours = check_colours(guess) - correct_exact
 
         if correct_exact == 4:
-            print("You win - you guessed the sequence!")
+            print(f"You win - you guessed the sequence! It was {sequence}.")
             break
 
-        print(f"Guess {guess_count}:")
+        print(f"\nGuess {guess_count}:")
         print(f"{correct_exact} of your colours are correct and in the right position.")
         print(f"{correct_colours} of your colours are correct but in the wrong position.\n")
 
@@ -25,18 +27,34 @@ def main_loop(sequence):
         print(f"You lose - you didn't guess the sequence! It was {sequence}.")
 
 
-def check_exact():
-    pass
+def check_exact(guess):
+
+    count = 0
+
+    for i in range(len(guess)):
+        if guess[i] == sequence[i]:
+            count += 1
+    
+    return count
 
 
-def check_colours():
-    pass
+def check_colours(guess):
+    
+    count = 0
+
+    counted_colours = []
+
+    for i in range(len(guess)):
+        for j in range(len(sequence)):
+            if guess[i] == sequence[j] and j not in counted_colours:
+                count += 1
+                counted_colours.append(j)
+                break
+
+    return count
 
     
 colours = ["R", "G", "B", "Y", "O", "P"]
-
-sequence = ""
-for _ in range(4):
-    sequence += random.choice(colours)
+sequence = [random.choice(colours) for _ in range(4)]
 
 main_loop(sequence)
